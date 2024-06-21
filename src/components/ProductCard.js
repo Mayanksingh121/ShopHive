7c8c5d6de4109f64638f321906715199c99fca3d
Mayank Singh <mt029322@gmail.com>
2024-06-21 20:59:22 +0530

    bug fix

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -24,7 +24,11 @@ const ProductCard = ({ product }) => {
   return (
     <div className="rounded-2xl border dark:border-[#232325] shadow-md w-44 md:w-60">
       <div className="bg-gradient-to-br from-[#5372b1] to-[#0e3a93] w-44 md:w-60 flex items-center justify-center rounded-t-2xl">
-        <img className="h-24 md:h-36" src={product.images[0]} alt="productImage"></img>
+        <img
+          className="h-24 md:h-36"
+          src={product.images[0]}
+          alt="productImage"
+        ></img>
       </div>
       <div className="dark:bg-[#19191a] p-2">
         <h3 className="font-body font-medium text-[#5e666b] dark:text-white h-16 md:h-12 text-sm">
@@ -34,16 +38,15 @@ const ProductCard = ({ product }) => {
           <span className="md:mr-2 px-2 py-1 rounded-xl text-xs md:text-sm border text-[#5e666b] dark:text-white border-[#d8f1ee] dark:border-white">
             {product.tags[0]}
           </span>
-          {product.tags[1] && <span className="px-2 py-1 rounded-xl text-xs md:text-sm border text-[#5e666b] dark:text-white border-[#d8f1ee]">
-            {product.tags[1]}
-          </span>}
         </p>
         <div className="flex justify-between items-center my-4 pr-1">
           <p className="font-bold text-sm md:text-lg">${product.price}</p>
           <button
             onClick={handleClick}
             className={`${
-              itemAdded ? "bg-[#f63a2f]" : "bg-gradient-to-br from-[#5372b1] to-[#0e3a93]"
+              itemAdded
+                ? "bg-[#f63a2f]"
+                : "bg-gradient-to-br from-[#5372b1] to-[#0e3a93]"
             } rounded-sm px-2 py-2 border-[1px] text-sm md:text-medium dark:border-0 font-medium  text-white`}
           >
             {itemAdded ? "Remove Item" : "Add to cart"}
@@ -52,9 +55,7 @@ const ProductCard = ({ product }) => {
       </div>
       <div className="dark:bg-[#19191a] w-full rounded-b-xl flex justify-center">
         <Link to={"/products/" + product.id}>
-          <button
-            className="text-[#5e666b] dark:text-white cursor-pointer text-center font-body font-medium"
-          >
+          <button className="text-[#5e666b] dark:text-white cursor-pointer text-center font-body font-medium">
             Get Product Details
           </button>
         </Link>
