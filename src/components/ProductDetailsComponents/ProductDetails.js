1036fcd65463028e5b9363081626602e0301c0a8
Mayank Singh <mt029322@gmail.com>
2024-06-21 20:57:31 +0530

    made product details component responsive

--- src/components/ProductDetailsComponents/ProductDetails.js
+++ src/components/ProductDetailsComponents/ProductDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { FaStar } from "react-icons/fa";
 import { useParams } from "react-router-dom";
-import Shimmer from "../Shimmer";
 import useGetAllProducts from "../../hooks/useGetAllProducts";
 import ProductReviewCard from "./ProductReviewCard";
 import { toast } from "react-hot-toast";
@@ -31,22 +31,15 @@ const ProductDetails = () => {
   };
 
   return (
-    <div className="dark:bg-[#111111] h-screen flex flex-col dark:text-white px-4">
+    <div className="dark:bg-[#111111] md:h-screen w-[100%] flex flex-col dark:text-white px-4">
       <h2 className="my-4 font-display text-center text-3xl">
         Product Details
       </h2>
       {reqProduct ? (
-        <div className="flex font-body gap-5">
-          <div>
-            <div className="bg-white h-100 rounded-lg">
-              <img className="h-96" alt="product" src={reqProduct.images[0]} />
-            </div>
-          </div>
-          <div className="mt-2 px-2">
+        <div className="flex flex-col md:flex-row font-body gap-5">
+          <div className="flex flex-col mt-2 md:pl-4 md:pr-10 md:w-1/2">
             {reqProduct.brand && (
-              <h3 className="font-medium text-2xl">
-                Brand: {reqProduct.brand}
-              </h3>
+              <h3 className="font-bold md:text-3xl">Brand: {reqProduct.brand}</h3>
             )}
             <p className="my-4">
               <span className="mr-2 px-2 py-1 rounded-xl text-sm border border-[#d8f1ee]">
@@ -56,25 +49,28 @@ const ProductDetails = () => {
                 {reqProduct.tags[1]}
               </span>
             </p>
-            <p className="mb-4">
-              <span className="font-medium text-xl">About the Product: </span>
-              <br />
-              <span className="text-lg">{reqProduct.description}</span>
-            </p>
+            <div className="flex justify-between w-full lg:w-2/3 my-7 md:mt-7  md:mb-8">
+              <p className="font-semibold text-xl">${reqProduct.price}</p>
+              <p className="mb-5 flex items-center gap-2">
+                <span className="flex text-yellow-400">
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </span>
+                <span className="text-sm font-semibold">
+                  {reqProduct.rating} / 5.0
+                </span>
+              </p>
+            </div>
+
+            <p className="mb-4">{reqProduct.description}</p>
+            <p className="text-sm">{reqProduct.returnPolicy}</p>
             <p>
-              <span className="font-medium text-xl">Price:</span>{" "}
-              <span className="text-lg">${reqProduct.price}</span>
+              <span className="font-medium text-lg">Warranty: </span>
+              <span className="text-sm">{reqProduct.warrantyInformation}</span>
             </p>
-            <p className="mb-5">
-              <span className="font-medium text-xl">Rating: </span>
-              <span className="text-lg">{reqProduct.rating}/5</span>
-            </p>
-            <p className="text-lg">{reqProduct.returnPolicy}</p>
-            <p>
-              <span className="font-medium text-xl">Warranty: </span>
-              <span className="text-lg">{reqProduct.warrantyInformation}</span>
-            </p>
-            <div className="flex gap-5 items-center mt-2">
+            <div className="flex justify-between items-center mt-5 md:mt-2">
               <p className="text-lg">{reqProduct.availabilityStatus}</p>
               <button
                 onClick={handleClick}
@@ -85,17 +81,26 @@ const ProductDetails = () => {
                 {itemAdded ? "Remove Item" : "Add to cart"}
               </button>
             </div>
-            <p className="my-4 font-medium text-lg">Reviews: </p>
-            <div className="flex gap-5">
-              {reqProduct.reviews.map((review, index) => {
-                return <ProductReviewCard key={index} review={review} />;
-              })}
+          </div>
+          <div className="w-full md:w-1/2">
+            <div className="flex justify-center dark:bg-[#19191a] w-full rounded-xl">
+              <img className="h-96" alt="product" src={reqProduct.images[0]} />
             </div>
           </div>
         </div>
       ) : (
-        <Shimmer classes="h-screen w-full"></Shimmer>
+        <div className="w-full h-screen bg-gray-100"></div>
       )}
+      {reqProduct ? (
+        <div className="hidden md:block">
+          <p className="my-4 font-medium text-lg">Reviews: </p>
+          <div className="flex gap-5 ">
+            {reqProduct.reviews.map((review, index) => {
+              return <ProductReviewCard key={index} review={review} />;
+            })}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/ProductDetailsComponents/ProductReviewCard.js
+++ src/components/ProductDetailsComponents/ProductReviewCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProductReviewCard = ({ review }) => {
   return (
-    <div className="shadow-xl w-1/3 flex flex-col justify-center font-body rounded-lg border px-2 py-2 mb-2 hover:bg-gradient-to-br hover:bg-pink-100">
+    <div className="shadow-xl w-1/3 dark:bg-[#19191a] dark:border-[#202021] flex flex-col justify-center font-body rounded-lg border px-2 py-2 mb-2">
       <div className="flex gap-4 justify-between items-center">
         <p className="text-lg font-medium">{review.reviewerName}</p>
         <p>Rating: {review.rating}/5</p>
